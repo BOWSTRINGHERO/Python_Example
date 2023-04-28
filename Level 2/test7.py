48187f40b99d4e725dac87040c276dd16cc2afc3
Radio_Hero <129924216+BOWSTRINGHERO@users.noreply.github.com>
2023-04-28 09:22:39 +0900

    Add files via upload

---
+++ Level 2/test7.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+
+if a == b == c:
+    print(10000+a*1000)
+elif a == b:
+    print(1000+a*100)
+elif a == c:
+    print(1000+a*100)
+elif b == c:
+    print(1000+b*100)
+else:
+    print(100 * max(a, b, c))
